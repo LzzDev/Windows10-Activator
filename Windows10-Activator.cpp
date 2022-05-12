133bbb58fed383d8ff46255c382789c1e15ac8ee
Lzz <callumnewsome100@gmail.com>
2022-05-12 02:38:15 +0100

    Files

---
+++ Windows10-Activator.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+std::map<std::string, std::string> keys {
+    {"Home", "TX9XD-98N7V-6WMQ6-BX7FG-H8Q99"},
+    {"Home N", "3KHY7-WNT83-DGQKR-F7HPR-844BM"},
+    {"Home Single Language", "7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH"},
+    {"Home Country Specific", "PVMJN-6DFY6-9CCP6-7BKTT-D3WVR"},
+    {"Professional", "W269N-WFGWX-YVC9B-4J6C9-T83GX"},
+    {"Professional N", "MH37W-N47XK-V7XM9-C7227-GCQG9"},
+    {"Education", "NW6C2-QMPVW-D7KKK-3GKT6-VCFB2"},
+    {"Education N", "2WH4N-8QGBV-H22JP-CT43Q-MDWWJ"},
+    {"Enterprise", "NPPR9-FWDCX-D2C8J-H872K-2YT43"},
+    {"Enterprise N", "DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4"},
+};
+
+
+int main()
+{
+    std::cout << "[+] Windows 10 Activator" << std::endl;
+    std::cout << "\n[+] Select windows version:" << std::endl;
+
+    int idx = 1;
+    for (auto i = keys.begin(); i != keys.end(); ++i) {
+        std::cout << idx << " : " << i->first << std::endl;
+        ++idx;
+    }
+
+    std::cout << "\n[+] Enter number according to Windows version: ";
+
+    int os;
+    std::cin >> os;
+    if (os > 0) --os;
+
+    auto key = keys.begin();
+    std::advance(key, os);
+    std::cout << "Using key " << key->second << " for " << key->first << std::endl << std::endl;
+
+    std::string installLicenseKeyCmd = "slmgr /ipk " + key->second;
+    std::string setKMSMachineAddressCmd = "slmgr /skms s8.uk.to";
+    std::string activateWindowsCmd = "slmgr /ato";
+
+    std::cout << "[-] " << installLicenseKeyCmd << std::endl;
+    std::cout << "[-] " << setKMSMachineAddressCmd << std::endl;
+    std::cout << "[-] " << activateWindowsCmd << std::endl << std::endl;
+
+    system(installLicenseKeyCmd.c_str());
+    system(setKMSMachineAddressCmd.c_str());
+    system(activateWindowsCmd.c_str());
+
+    system("pause");
+}
